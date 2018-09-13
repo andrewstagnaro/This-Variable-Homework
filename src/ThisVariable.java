42f7ecb14e62e0ef79a69405489d5357cc04f9c2
andrewstagnaro <42977270+andrewstagnaro@users.noreply.github.com>
2018-09-13 13:37:57 -0400

    Add files via upload

---
+++ src/ThisVariable.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @author andrew stagnaro
+ *
+ */
+public class ThisVariable 
+{
+
+	public static void main(String[] args) 
+	{
+	String person="Drew";
+	System.out.println(person);
+	int length;
+	length=4;
+	int width;
+	width=2;
+	
+	
+	
+	
+	
+
+	
+	
+	
+
+	}
+
+}
